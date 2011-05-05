30f003a8e46d696c70affb40b19b683360a7ada8
Andrea Reginato <andrea.reginato@gmail.com>
2011-05-05 09:11:57 +0200

    Added webmock call to type with returning JSON

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,13 +1,11 @@
 # URI to stub
 uri = Settings.type_stub.host + Settings.type_stub.path
 
-## Stubbing
-# Type request
-stub_request(:get, uri).to_return(body: {name: "Dimmer"})
+# Type Stubbing
+stub_request(:get, uri).to_return(body: Settings.type_json)
 
 # Request
 res = Net::HTTP.get( Settings.type_stub.host, Settings.type_stub.path)
 
 # Result
-puts ":::" + res.inspect
-puts ":::" + res.class.inspect
+puts ":::" + JSON.parse(res).inspect
