c19929f318a61964d98cff1c5bc8dbfba0c83c3e
Andrea Reginato <andrea.reginato@gmail.com>
2011-05-08 12:32:33 +0200

    Attempt to create a custom matcher

--- spec/models/pending_property_spec.rb
+++ spec/models/pending_property_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe PendingProperty do
-  it { should validate_presence_of(:value) }
-  it { should validate_presence_of(:old_value) }
- 
+  it { should validate_presence_of :value }
+  it { should validate_presence_of :old_value }
+
   it { should allow_value(Settings.validation.valid_uri).for(:uri) }
   it { should_not allow_value(Settings.validation.not_valid_uri).for(:uri) } 
 end
