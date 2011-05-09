acccc3457658ab34a399e98f7be46070ea594a2a
Andrea Reginato <andrea.reginato@gmail.com>
2011-05-09 10:51:07 +0200

    Completed tests on #normalize_timingin  consumption model

--- app/models/consumption.rb
+++ app/models/consumption.rb
@@ -6,11 +6,11 @@ class Consumption
   field :uri
   field :created_from
   field :type, default: 'instantaneous'
-  field :consumption, type: Float, default: 0.0
+  field :consumption, type: Float
   field :unit, default: 'kwh'
   field :occur_at, type: Time
   field :end_at, type: Time
-  field :duration, type: Float, default: 0.0
+  field :duration, type: Float
 
   attr_accessible :type, :energy, :unit, :occur_at, :end_at, :duration
 
@@ -23,24 +23,18 @@ class Consumption
   validates :end_at, presence: true, if: :durational?
   validates :duration, presence: true, if: :durational?
   
+  before_validation :normalize_timings
 
-  # Normalize timings when the measurament type is durational
-  # so that we can have occur_at, end_at and duration also if
-  # one of them is missing.
+
+  # Normalize timings when the measurament type is durational so
+  # that we can have occur_at, end_at and duration also if one of
+  # them is missing. When more than one is missing an error is raised
   def normalize_timings
-    puts "::::::::::::::::::::::::::::::::::::"
-    puts ":::: occur_at " + occur_at.inspect
-    puts ":::: end_at " + end_at.inspect
-    puts ":::: duration " + duration.inspect
-
-    self.end_at   = calculate_end_at   if (occur_at and duration and end_at.nil?)
-    self.occur_at = calculate_occur_at if (end_at and duration and occur_at.nil?)
-    self.duration = calculate_duration if (occur_at and end_at and duration.nil?)
-
-    puts ":::: occur_at " + occur_at.inspect
-    puts ":::: end_at " + end_at.inspect
-    puts ":::: duration " + duration.inspect
-    puts "::::::::::::::::::::::::::::::::::::"
+    if durational?
+      self.end_at   = calculate_end_at   if (occur_at and duration and end_at.nil?)
+      self.occur_at = calculate_occur_at if (end_at and duration and occur_at.nil?)
+      self.duration = calculate_duration if (occur_at and end_at and duration.nil?)
+    end
   end
 
   private 
--- spec/models/consumption_spec.rb
+++ spec/models/consumption_spec.rb
@@ -17,51 +17,56 @@ describe Consumption do
   it { should validate_presence_of(:occur_at) }
 
   context "#normalize_timings" do
-    before { @correct = Factory(:durational_consumption) }
+    context "with durational consumption" do
+      before { @correct = Factory(:durational_consumption) }
 
-    context "with all timing present" do
-      before { @consumption = Factory(:durational_consumption) }
-      before { @consumption.normalize_timings }
-      before { @consumption.save! }
-      it "does not change timings" do
-        @consumption.occur_at.should == @correct.occur_at
-        @consumption.end_at.should == @correct.end_at
-        @consumption.duration.should == @correct.duration
+      context "with all timing present" do
+        before { @consumption = Factory(:durational_consumption) }
+        before { @consumption.save! }
+        it "does not change timings" do
+          @consumption.occur_at.should == @correct.occur_at
+          @consumption.end_at.should == @correct.end_at
+          @consumption.duration.should == @correct.duration
+        end
+      end
+
+      context "with #duration missing" do
+        before { @consumption = Factory.build(:durational_consumption, duration: nil) }
+        before { @consumption.save! }
+        it "calculates duration field" do
+          @consumption.duration.should == @correct.duration
+        end
+      end
+
+      context "with #end_at missing" do
+        before { @consumption = Factory.build(:durational_consumption, end_at: nil) }
+        before { @consumption.save! }
+        it "calculates end_at field" do
+          @consumption.end_at.should == @correct.end_at
+        end
+      end
+
+      context "with #occur_at missing" do
+        before { @consumption = Factory.build(:durational_consumption, occur_at: nil) }
+        before { @consumption.save! }
+        it "calculates occur_at field" do
+          @consumption.occur_at.should == @correct.occur_at
+        end
+      end
+
+      context "with two timings missing" do
+        before { @consumption = Factory.build(:durational_consumption, occur_at: nil, duration: nil) }
+        it "raise error" do
+          lambda{ @consumption.save! }.should raise_error
+        end
       end
     end
 
-    context "with #duration missing" do
-      before { @consumption = Factory.build(:durational_consumption, duration: nil) }
-      before { @consumption.normalize_timings }
-      before { @consumption.save! }
-      it "calculates duration field" do
-        @consumption.duration.should == @correct.duration
-      end
-    end
-
-    context "with #end_at missing" do
-      before { @consumption = Factory.build(:durational_consumption, end_at: nil) }
-      before { @consumption.normalize_timings }
-      before { @consumption.save! }
-      it "calculates end_at field" do
-        @consumption.end_at.should == @correct.end_at
-      end
-    end
-
-    context "with #occur_at missing" do
-      before { @consumption = Factory.build(:durational_consumption, occur_at: nil) }
-      before { @consumption.normalize_timings }
-      before { @consumption.save! }
-      it "calculates occur_at field" do
-        @consumption.occur_at.should == @correct.occur_at
-      end
-    end
-
-    context "with two timings missing" do
-      before { @consumption = Factory.build(:durational_consumption, occur_at: nil, duration: nil) }
-      before { @consumption.normalize_timings }
-      it "raise error" do
-        lambda{ @consumption.save! }.should raise_error
+    context "with instantaneous consumption" do
+      before  { @consumption = Factory(:consumption) }
+      it "is not called" do
+        @consumption.end_at.should be_nil
+        @consumption.duration.should be_nil
       end
     end
   end
