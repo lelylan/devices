3f622eaf9406f016f31ecb2b5e3685d229f880d3
Andrea Reginato <andrea.reginato@gmail.com>
2011-05-21 14:25:25 +0200

    Removed status

--- spec/models/device_status_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe DeviceStatus do
-  it { should validate_presence_of(:name) }
-  it { should allow_value(Settings.validation.valid_uri).for(:uri) }
-  it { should_not allow_value(Settings.validation.not_valid_uri).for(:uri) }
-end
