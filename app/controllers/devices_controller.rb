d40937e27bd5ab0352fbfd9ef38b599aa5804460
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-07 12:28:27 +0200

    Added device controller

---
+++ app/controllers/devices_controller.rb
@@ -0,0 +1,68 @@
+class DevicesController < ApplicationController
+  doorkeeper_for :index, :show, scopes: [:read, :write]
+  doorkeeper_for :create, :update, :destroy, scopes: [:write]
+
+  before_filter :find_owned_resources
+  before_filter :find_resource, only: %w(show update destroy)
+  before_filter :search_params, only: %w(index)
+  before_filter :pagination,    only: %w(index)
+
+  def index
+    @devices = @devices.limit(params[:per])
+  end
+
+  def show
+  end
+
+  def create
+    @device = Device.new(params)
+    @device.resource_owner_id = current_user.id
+    if @device.save!
+      render 'show', status: 201, location: DeviceDecorator.decorate(@device).uri
+    else
+      render_422 'notifications.resource.not_valid', @device.errors
+    end
+  end
+
+  def update
+    if @device.update_attributes!(params)
+      render 'show'
+    else
+      render_422 'notifications.resource.not_valid', @device.errors
+    end
+  end
+
+  def destroy
+    render 'show'
+    @device.destroy
+  end
+
+  private
+
+  def find_owned_resources
+    @devices = Device.where(resource_owner_id: current_user.id)
+  end
+
+  def find_resource
+    @device = @devices.find(params[:id])
+  end
+
+  def search_params
+    @devices = @devices.where('name'    => /.*#{params[:name]}.*/i) if params[:name]
+    @devices = @devices.where('type_id' => find_id(params[:type]))  if params[:type]
+
+    if params[:property] and params[:value]
+      @devices = @devices.where('properties' => { '$elemMatch' => { id: find_id(params[:property]), value: params[:value] } })
+    else
+      @devices = @devices.where('properties.id'   => params[:property_uri])   if params[:property_uri]
+      @devices = @devices.where('properties.value' => params[:property_value]) if params[:property_value]
+    end
+  end 
+
+  def pagination
+    params[:per] = (params[:per] || Settings.pagination.per).to_i
+    params[:per] = Settings.pagination.per if params[:per] == 0 
+    params[:per] = Settings.pagination.max_per if params[:per] > Settings.pagination.max_per
+    @devices = @devices.gt(id: find_id(params[:start])) if params[:start]
+  end
+end
---
+++ spec/requests/devices_controller_spec.rb
@@ -0,0 +1,73 @@
+require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
+
+feature 'DevicesController' do
+
+  let!(:application)  { FactoryGirl.create :application }
+  let!(:user)         { FactoryGirl.create :user }
+  let!(:access_token) { FactoryGirl.create :access_token, application: application, scopes: 'write', resource_owner_id: user.id }
+
+  before { page.driver.header 'Authorization', "Bearer #{access_token.token}" }
+  before { page.driver.header 'Content-Type', 'application/json' }
+
+  let(:controller) { 'devices' }
+  let(:factory)    { 'device' }
+
+  describe 'GET /devices' do
+
+    let!(:resource)  { FactoryGirl.create :device, resource_owner_id: user.id }
+    let(:uri)        { '/devices' }
+
+    it_behaves_like 'a listable resource'
+    it_behaves_like 'a paginable resource'
+    it_behaves_like 'a searchable resource', { name: 'My name is resource' }
+
+    # TODO add tests on search type
+    # TODO add tests on search properties
+  end
+
+  context 'GET /devices/:id' do
+
+    let!(:resource) { FactoryGirl.create :device, resource_owner_id: user.id }
+    let(:uri)       { "/devices/#{resource.id}" }
+
+    it_behaves_like 'a showable resource'
+    it_behaves_like 'a changeable host'
+    it_behaves_like 'a not owned resource', 'page.driver.get(uri)'
+    it_behaves_like 'a not found resource', 'page.driver.get(uri)'
+  end
+
+  context 'POST /devices' do
+
+    let(:uri)    { '/devices' }
+    let(:device) { FactoryGirl.create 'device' }
+    let(:type)   { FactoryGirl.create 'type' }
+    let(:params) { { name: 'Dimmer', type: a_uri(type) } }
+
+    before         { page.driver.post uri, params.to_json }
+    let(:resource) { Device.last }
+
+    it_behaves_like 'a creatable resource'
+    it_behaves_like 'a validated resource', 'page.driver.post(uri, {}.to_json)', { method: 'POST', error: 'can\'t be blank' }
+  end
+
+  context 'PUT /devices/:id' do
+
+    let!(:resource) { FactoryGirl.create :device, resource_owner_id: user.id }
+    let(:uri)       { "/devices/#{resource.id}" }
+    let(:params)    { { name: 'Updated' } }
+
+    it_behaves_like 'an updatable resource'
+    it_behaves_like 'a not owned resource', 'page.driver.put(uri)'
+    it_behaves_like 'a not found resource', 'page.driver.put(uri)'
+    it_behaves_like 'a validated resource', 'page.driver.put(uri, { name: "" }.to_json)', { method: 'PUT', error: 'can\'t be blank' }
+  end
+
+  context 'DELETE /devices/:id' do
+    let!(:resource)  { FactoryGirl.create :device, resource_owner_id: user.id }
+    let(:uri)        { "/devices/#{resource.id}" }
+
+    it_behaves_like 'a deletable resource'
+    it_behaves_like 'a not owned resource', 'page.driver.delete(uri)'
+    it_behaves_like 'a not found resource', 'page.driver.delete(uri)'
+  end
+end
