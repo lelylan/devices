7f8b959617cfeb534760da50a4d6e27e508a8b2e
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-27 14:03:04 +0200

    Reomved action_mailer as not used

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 #require 'rails/all'
 require 'action_controller/railtie'
-require 'action_mailer/railtie'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
