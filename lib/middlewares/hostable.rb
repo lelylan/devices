bab36329ad2328953c78f61735375796793e4042
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-28 11:46:47 +0200

    Added debug info on middleware to debug

--- lib/middlewares/hostable.rb
+++ lib/middlewares/hostable.rb
@@ -4,7 +4,10 @@ class Hostable
   end
 
   def call(env)
+    puts "REGGIE LOG"
+    puts env
     env['HTTP_HOST'] = env['HTTP_X_HOST'] if env['HTTP_X_HOST']
+    puts env['HTTP_HOST']
     @app.call(env)
   end
 end
