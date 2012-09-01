b90a0b42f9afd8153cc115d63b0c456374ee8ae3
Andrea Reginato <andrea.reginato@gmail.com>
2012-09-02 00:55:40 +0200

    Resized number of unicorn workers

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -224,7 +224,7 @@ describe Device do
 
       let(:parsed) { resource.device_properties(properties).first }
 
-      it { parsed[:id].should    == status.id.to_s }
+      it { parsed[:id].should == status.id.to_s }
       it { parsed[:value].should == 'on' }
       it { parsed.should_not have_key(:physical) }
     end
@@ -233,7 +233,7 @@ describe Device do
 
       let(:parsed) { resource.device_properties(properties).last }
 
-      it { parsed[:id].should       == intensity.id.to_s }
+      it { parsed[:id].should  == intensity.id.to_s }
       it { parsed.should_not have_key(:value) }
       it { parsed[:physical].should == '20' }
     end
