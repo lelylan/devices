3d72136e604d3557b839f03e6614f6fe5da4710b
Andrea Reginato <andrea.reginato@gmail.com>
2012-09-01 12:23:19 +0200

    Set correct collection names for doorkeeper

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -5,8 +5,8 @@ end
 
 # Doorkeeper models extensions
 Devices::Application.config.to_prepare do
-  Doorkeeper::AccessToken.class_eval { store_in session: 'people' }
-  Doorkeeper::AccessGrant.class_eval { store_in session: 'people' }
-  Doorkeeper::Application.class_eval { store_in session: 'people' }
+  Doorkeeper::AccessToken.class_eval { store_in collection: :oauth_access_tokens, session: 'people' }
+  Doorkeeper::AccessGrant.class_eval { store_in collection: :oauth_access_grants, session: 'people' }
+  Doorkeeper::Application.class_eval { store_in collection: :oauth_access_applications, session: 'people' }
   Doorkeeper::AccessToken.class_eval { include Filterable }
 end
