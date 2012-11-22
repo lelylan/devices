511e8486a47a477b71e1770e661aea50eea5ecbe
Andrea Reginato <andrea.reginato@gmail.com>
2012-11-22 17:04:27 +0100

    Added encrypted_password field to model

--- app/models/people/user.rb
+++ app/models/people/user.rb
@@ -1,4 +1,7 @@
 class User
   include Mongoid::Document
   store_in session: 'people'
+
+  field :email,              type: String, default: ''
+  field :encrypted_password, type: String, default: ''
 end
