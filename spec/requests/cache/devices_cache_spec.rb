627997bfabf7d11fd6890ad8b8ed4ef21cac956c
Andrea Reginato <andrea.reginato@gmail.com>
2012-11-29 17:04:01 +0100

    Removed comments on device caching tests

--- spec/requests/cache/devices_cache_spec.rb
+++ spec/requests/cache/devices_cache_spec.rb
@@ -16,100 +16,100 @@ feature 'Caching' do
   let(:controller) { 'devices' }
   let(:factory)    { 'device' }
 
-  #describe 'GET /devices/:id' do
+  describe 'GET /devices/:id' do
 
-    #let!(:resource) { FactoryGirl.create :device, resource_owner_id: user.id }
-    #let(:uri)       { "/devices/#{resource.id}" }
-    #let(:cache_key) { ActiveSupport::Cache.expand_cache_key(['device_serializer', resource.cache_key, 'to-json']) }
+    let!(:resource) { FactoryGirl.create :device, resource_owner_id: user.id }
+    let(:uri)       { "/devices/#{resource.id}" }
+    let(:cache_key) { ActiveSupport::Cache.expand_cache_key(['device_serializer', resource.cache_key, 'to-json']) }
 
-    #before { page.driver.get uri }
+    before { page.driver.get uri }
 
-    #describe 'with fragment caching' do
+    describe 'with fragment caching' do
 
-      #it 'creates the fragment cache' do
-        #Rails.cache.exist?(cache_key).should be_true
-      #end
+      it 'creates the fragment cache' do
+        Rails.cache.exist?(cache_key).should be_true
+      end
 
-      #it 'saves the JSON resource into the cache' do
-        #cached = JSON.parse Rails.cache.read(cache_key)
-        #has_resource resource, cached
-      #end
-    #end
+      it 'saves the JSON resource into the cache' do
+        cached = JSON.parse Rails.cache.read(cache_key)
+        has_resource resource, cached
+      end
+    end
 
-    #describe 'with HTTP caching' do
+    describe 'with HTTP caching' do
 
-      #describe 'when sends the If-Modified-Since header' do
+      describe 'when sends the If-Modified-Since header' do
 
-        #describe 'when the resource does not change' do
+        describe 'when the resource does not change' do
 
-          #before { page.driver.header 'If-Modified-Since', resource.updated_at.httpdate }
-          #before { page.driver.get uri }
+          before { page.driver.header 'If-Modified-Since', resource.updated_at.httpdate }
+          before { page.driver.get uri }
 
-          #it 'returns a not modified response' do
-            #page.status_code.should == 304
-          #end
-        #end
+          it 'returns a not modified response' do
+            page.status_code.should == 304
+          end
+        end
 
-        #describe 'when the resource changes' do
+        describe 'when the resource changes' do
 
-          #let!(:timestamp) { resource.updated_at }
+          let!(:timestamp) { resource.updated_at }
 
-          #before { resource.updated_at = resource.updated_at + 1; resource.save }
-          #before { page.driver.header 'If-Modified-Since', (timestamp).httpdate }
-          #before { page.driver.get uri }
+          before { resource.updated_at = resource.updated_at + 1; resource.save }
+          before { page.driver.header 'If-Modified-Since', (timestamp).httpdate }
+          before { page.driver.get uri }
 
-          #it 'executes the action' do
-            #page.status_code.should == 200
-          #end
+          it 'executes the action' do
+            page.status_code.should == 200
+          end
 
-          #it 'creates a new fragment cache' do
-            #new_key = ActiveSupport::Cache.expand_cache_key(['device_serializer', resource.cache_key, 'to-json'])
-            #Rails.cache.exist?(new_key).should be_true
-          #end
+          it 'creates a new fragment cache' do
+            new_key = ActiveSupport::Cache.expand_cache_key(['device_serializer', resource.cache_key, 'to-json'])
+            Rails.cache.exist?(new_key).should be_true
+          end
 
-          #it 'returns the Last-Modified header' do
-            #page.response_headers['Last-Modified'].should == resource.updated_at.httpdate
-          #end
-        #end
-      #end
+          it 'returns the Last-Modified header' do
+            page.response_headers['Last-Modified'].should == resource.updated_at.httpdate
+          end
+        end
+      end
 
-      #describe 'when sends the If-None-Match header' do
+      describe 'when sends the If-None-Match header' do
 
-        #let(:etag) { page.response_headers['ETag'] }
+        let(:etag) { page.response_headers['ETag'] }
 
-        #describe 'when the resource does not change' do
+        describe 'when the resource does not change' do
 
-          #before { page.driver.header 'If-None-Match', etag }
-          #before { page.driver.get uri }
+          before { page.driver.header 'If-None-Match', etag }
+          before { page.driver.get uri }
 
-          #it 'returns a not modified response' do
-            #page.status_code.should == 304
-          #end
-        #end
+          it 'returns a not modified response' do
+            page.status_code.should == 304
+          end
+        end
 
-        #describe 'when the resource changes' do
+        describe 'when the resource changes' do
 
-          #before { resource.updated_at = resource.updated_at + 1; resource.save }
-          #before { page.driver.header 'If-None-Match', etag }
-          #before { page.driver.get uri }
+          before { resource.updated_at = resource.updated_at + 1; resource.save }
+          before { page.driver.header 'If-None-Match', etag }
+          before { page.driver.get uri }
 
-          #it 'executes the action' do
-            #page.status_code.should == 200
-          #end
+          it 'executes the action' do
+            page.status_code.should == 200
+          end
 
-          #it 'creates a new fragment cache' do
-            #new_key = ActiveSupport::Cache.expand_cache_key(['device_serializer', resource.cache_key, 'to-json'])
-            #Rails.cache.exist?(new_key).should be_true
-          #end
+          it 'creates a new fragment cache' do
+            new_key = ActiveSupport::Cache.expand_cache_key(['device_serializer', resource.cache_key, 'to-json'])
+            Rails.cache.exist?(new_key).should be_true
+          end
 
-          #it 'returns the ETag header' do
-            #page.response_headers['ETag'].should_not == etag
-          #end
-        #end
-      #end
-    #end
-  #end
+          it 'returns the ETag header' do
+            page.response_headers['ETag'].should_not == etag
+          end
+        end
+      end
+    end
+  end
 
-  # TODO: does not work
+  # TODO: does not work with serializers
   #describe 'GET /devices/:id/privates' do
 end
