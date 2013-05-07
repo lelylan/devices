68a55d78a95fbfe436b97f9837b9a5b61defea46
Andrea Reginato <andrea.reginato@gmail.com>
2013-05-07 16:03:07 +0200

    Refactoring comments

--- spec/models/device_property_spec.rb
+++ spec/models/device_property_spec.rb
@@ -7,7 +7,8 @@ describe DeviceProperty do
 
   it { should validate_presence_of :property_id }
 
-  # Here we do not use the connection shared example. The reason is that this connection
-  # is way more complex than the others and we need additional special cases that are
-  # covered in the device_spec test suite.
+  # Here we do not use the connection shared example.
+  #
+  # The reason is that this connection is way more complex than the others and we
+  # need special test cases that are covered in the device_spec test suite.
 end
