58ee2013f938c6709432cbd6638c3c22c029c511
Andrea Reginato <andrea.reginato@gmail.com>
2013-05-13 16:17:16 +0200

    Added some tests to the user model

--- spec/models/people/user_spec.rb
+++ spec/models/people/user_spec.rb
@@ -11,4 +11,32 @@ describe User do
   it 'creates a user' do
     user.id.should_not be_nil
   end
+
+  describe '#description' do
+
+    describe 'when has email' do
+
+      before { user.update_attributes(username: nil, full_name: nil) }
+
+      it 'get the email' do
+        user.description.should == 'alice@example.com'
+      end
+    end
+
+    describe 'when has username' do
+
+      before { user.update_attributes(full_name: nil) }
+
+      it 'get the email' do
+        user.description.should == 'alice'
+      end
+    end
+
+    describe 'when has fullname' do
+
+      it 'get the email' do
+        user.description.should == 'Alice Wonderland'
+      end
+    end
+  end
 end
